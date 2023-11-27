d5c6c9718f62d73ca2e5c5046a892beb4d792b85
ning2413 <82848395+ning2413@users.noreply.github.com>
2023-11-26 20:22:47 -0500

    Change name

--- testing/test.sh
+++ testing/test.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
 # Number of times to run the scripts
-num_runs=10
+num_runs=1
 echo "NONE RANDOM ALL RIGHT"
 # Loop to run the scripts multiple times
 for ((i=1; i<=num_runs; i++)); do
     echo "Run #$i"
 
     # Run generate_bins.sh
-    ./generate_bins.sh
+    ./ABsameBins.sh
     # Run test_correct.sh
-    ./test_correct.sh
+    ./testOutput.sh
 
     echo "Completed run #$i"
     echo "-----------------------"
-    sleep 10
 done
+
 echo "ALL RANDOM ALL WRONG"
 for ((i=num_runs + 1; i<=2*num_runs; i++)); do
     echo "Run #$i"
 
     # Run generate_bins.sh
-    ./generate_bins2.sh
+    ./ABdiffBins.sh
     # Run test_correct.sh
-    ./test_correct.sh
+    ./testOutput.sh
 
     echo "Completed run #$i"
     echo "-----------------------"
-    sleep 10
 done
 echo "Finished all runs."
